d6d757dfc48c5d04f2789eedd3579da21e5cc78a
ValliT <31540119+ValliT@users.noreply.github.com>
2017-10-15 08:03:14 -0700

    Add files via upload

---
+++ AboutMyself.java
@@ -0,0 +1,22 @@
+public class AboutMyself {
+
+	public static void main(String[] args) {
+		
+		// Developed by : Valli Dorai       Date: 10/15/17
+		
+		System.out.println("\t\t\t\t         Murugavalli(Valli) Dorai      \n\t\t\t\t         ------------------------      \n");
+		System.out.println("\tI\'m an IT Consultant and Computer Science graduate with 6+ years of extensive work experience as");
+		System.out.print("an Analyst/Database Developer/Test Engineer and volunteering experience as a Team Lead.");
+		System.out.println("  With effective \ninterpersonal/written/verbal skills and a passion for Software Development, I am in the outlook for the");
+		System.out.println("position of SDE.  \n\n\tHaving been part of the complete SDLC in all my projects, I bring in Analytical skills,");
+		System.out.println("Quick problem solving skills, Organizational skills, Trustworthiness with a positive attitude. A quick  ");
+		System.out.print("learner and observer with great listening skills and paying attention to detail have been my invaluable \nskillset.  ");
+		System.out.println("This makes me a reliable individual to work independently and also a good team player.  ");
+		System.out.println("Striving for quality excellence in all my tasks have always been my primary goal.");
+		
+		
+	}
+	
+	
+
+}
